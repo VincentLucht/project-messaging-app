8c865b04634856025db79b04600f70596d2d76c4
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-04 13:07:36 +0200

    Add socket.io

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,55 @@
+import { useState, useEffect } from 'react';
+import { io, Socket } from 'socket.io-client';
+
 export default function App() {
-  return <div className="">I&apos;m a template</div>;
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<string[]>([]);
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    // connect socket to backend
+    const socket = io('ws://localhost:3000');
+
+    // Listen for incoming messages from the server
+    socket.on('message', (data: string) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+
+    setSocket(socket); // Store the WebSocket instance in state
+
+    // ? Cleanup WebSocket on component unmount
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (message !== '' && socket) {
+      socket.emit('message', message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={(e) => sendMessage(e)}>
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button>Send message</button>
+      </form>
+
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index} className="mb-1">
+            {message}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
