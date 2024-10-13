698f39becbf19f927ac42eda942954c50a32b8bc
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-13 22:01:50 +0200

    Move httpServer into other file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,15 @@ import express from 'express';
 import router from './routes/router';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+
+import { createServer } from 'http';
+import { setupSocketIO } from './server/server';
 dotenv.config();
+
 const app = express();
+// Create server
+const httpServer = createServer(app);
+setupSocketIO(httpServer);
 
 // Configure CORS globally
 app.use(
@@ -13,13 +20,14 @@ app.use(
     credentials: true, // ! allow for portfolio showcase
   }),
 );
-
 app.use(express.json()); // ? parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // ? allow req.body
 
 app.use('/', router);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on Port 3000');
+httpServer.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
 });
+
+export default app;
