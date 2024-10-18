c0613d6b59256a9acdf5cdae83e978ea61713ad7
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-18 13:55:31 +0200

    Remove faulty middleware

--- backend/src/auth/authController.ts
+++ backend/src/auth/authController.ts
@@ -38,13 +38,12 @@ class AuthController {
     try {
       const user = await prisma.user.findUnique({
         where: {
-          name: req.body.name,
           username: req.body.username,
         },
       });
 
       if (user && (await bcrypt.compare(req.body.password, user.password))) {
-        const payload = { id: user.id, name: user.name };
+        const payload = { id: user.id, username: user.username };
         const token = jwt.sign(payload, this.secretKey, { expiresIn: '14 days' });
         return res.json({ token: `Bearer ${token}` });
       } else {
@@ -54,24 +53,6 @@ class AuthController {
       return res.status(500).json({ error });
     }
   }
-
-  verifyUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const userId = req.body.user_id || req.params.user_id;
-    const user = await db.getUserById(userId);
-    if (!user) {
-      return res.status(400).json({ message: 'Invalid user ID' });
-    }
-    next();
-  });
-
-  verifyChat = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const chatId = req.body.chat_id || req.params.chat_id;
-    const chat = await db.getChatById(chatId);
-    if (!chat) {
-      return res.status(404).json({ message: 'Invalid chat ID' });
-    }
-    next();
-  });
 }
 
 const authController = new AuthController();
