20a46fc7c47e7bdbef0c07ab0982f4a4a49d8aec
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-18 14:00:40 +0200

    Add chat creation util function

---
+++ frontend/src/app/main/chat/create chat/handleCreateChat.ts
@@ -0,0 +1,34 @@
+import { API_URL } from '../../../../App';
+import { ExpressErrors } from '../../../interfaces/express-validator-errors';
+
+export default async function handleCreateChat(
+  userId: string,
+  token: string,
+  chatName: string,
+  isGroupChat: boolean,
+  password?: string,
+) {
+  let isPasswordProtected: boolean;
+  password ? (isPasswordProtected = true) : (isPasswordProtected = false);
+
+  const response = await fetch(`${API_URL}/chat`, {
+    method: 'POST',
+    headers: {
+      Authorization: `${token}`,
+      'Content-Type': 'application/json',
+    },
+    // ? doesn't matter if password is undefined, backend will check if chat is password protected
+    body: JSON.stringify({
+      user_id: userId,
+      name: chatName,
+      is_group_chat: isGroupChat,
+      password,
+      is_password_protected: isPasswordProtected,
+    }),
+  });
+
+  if (!response.ok) {
+    const errorObject = (await response.json()) as ExpressErrors;
+    throw errorObject;
+  }
+}
