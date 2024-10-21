da88bb86ba93a55f4e74eebe3d118bc1d54b4a0a
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-21 13:19:17 +0200

    Add new method to get all user chats

--- backend/src/controller/chatController.ts
+++ backend/src/controller/chatController.ts
@@ -8,7 +8,13 @@ class ChatController {
   createChat = asyncHandler(async (req: Request, res: Response) => {
     if (checkValidationError(req, res)) return;
 
-    const { user_id, name, is_group_chat, chat_description, is_password_protected } = req.body;
+    const {
+      user_id,
+      name,
+      is_group_chat,
+      chat_description,
+      is_password_protected,
+    } = req.body;
     const { password = undefined } = is_password_protected ? req.body : {};
 
     try {
@@ -17,12 +23,22 @@ class ChatController {
         return res.status(404).json({ message: 'User ID not found' });
       }
 
-      const chat = await db.createChat(user_id, name, is_group_chat, chat_description, is_password_protected, password);
+      const chat = await db.createChat(
+        user_id,
+        name,
+        is_group_chat,
+        chat_description,
+        is_password_protected,
+        password,
+      );
       await db.makeUserAdminById(chat.id, user_id);
 
       return res.status(201).json({ message: 'Successfully created chat' });
     } catch (error) {
-      return res.status(500).json({ message: 'Failed to create chat', error: (error as Error).message });
+      return res.status(500).json({
+        message: 'Failed to create chat',
+        error: (error as Error).message,
+      });
     }
   });
 
@@ -35,7 +51,9 @@ class ChatController {
       // user can't add themselves
       const user = await db.getUserByUsername(username);
       if (!user) {
-        return res.status(404).json({ message: `Username ${username} not found` });
+        return res
+          .status(404)
+          .json({ message: `Username ${username} not found` });
       }
 
       // chat doesn't exist
@@ -47,14 +65,18 @@ class ChatController {
       // other user does not exist
       const otherUser = await db.getUserByUsername(other_username);
       if (!otherUser) {
-        return res.status(404).json({ message: `Username ${other_username} was not found` });
+        return res
+          .status(404)
+          .json({ message: `Username ${other_username} was not found` });
       }
 
       // other User is already in the chat
       const chatMembers = await db.getChatMembers(chat_id);
       const memberSet = new Set(chatMembers?.map((user) => user.username));
       if (memberSet.has(other_username)) {
-        return res.status(409).json({ message: `${other_username} is already part of the chat` });
+        return res
+          .status(409)
+          .json({ message: `${other_username} is already part of the chat` });
       }
 
       // is user admin of the chat
@@ -66,9 +88,14 @@ class ChatController {
       // ! TODO: Disallow adding/joining users if it is not a group chat
 
       await db.addUserToChat(otherUser.id, chat_id);
-      return res.status(201).json({ message: 'Successfully added user to chat' });
+      return res
+        .status(201)
+        .json({ message: 'Successfully added user to chat' });
     } catch (error) {
-      return res.status(500).json({ message: 'Failed to add user to chat', error: (error as Error).message });
+      return res.status(500).json({
+        message: 'Failed to add user to chat',
+        error: (error as Error).message,
+      });
     }
   });
 
@@ -81,7 +108,9 @@ class ChatController {
       // does other user exist
       const otherUser = await db.getUserByUsername(other_username);
       if (!otherUser) {
-        return res.status(404).json({ message: `Username ${other_username} not found` });
+        return res
+          .status(404)
+          .json({ message: `Username ${other_username} not found` });
       }
 
       const isAdmin = await db.isChatAdminById(chat_id, user_id);
@@ -89,16 +118,26 @@ class ChatController {
         return res.status(403).json({ message: 'You are not an admin' });
       }
 
-      const isOtherUserAdmin = await db.isChatAdminByUsername(chat_id, other_username);
+      const isOtherUserAdmin = await db.isChatAdminByUsername(
+        chat_id,
+        other_username,
+      );
       if (isOtherUserAdmin) {
-        return res.status(409).json({ message: `User ${other_username} already is an admin` });
+        return res
+          .status(409)
+          .json({ message: `User ${other_username} already is an admin` });
       }
 
       await db.makeUserAdminByUsername(chat_id, other_username);
 
-      return res.status(201).json({ message: `Successfully made user ${other_username} an admin` });
+      return res
+        .status(201)
+        .json({ message: `Successfully made user ${other_username} an admin` });
     } catch (error) {
-      return res.status(500).json({ message: 'Failed to make user admin', error: (error as Error).message });
+      return res.status(500).json({
+        message: 'Failed to make user admin',
+        error: (error as Error).message,
+      });
     }
   });
 
@@ -111,7 +150,33 @@ class ChatController {
       await db.createMessage(user_id, chat_id, content);
       return res.status(201).json({ message: 'Successfully created message' });
     } catch (error) {
-      return res.status(500).json({ message: 'Failed to create message', error: (error as Error).message });
+      return res.status(500).json({
+        message: 'Failed to create message',
+        error: (error as Error).message,
+      });
+    }
+  });
+
+  getAllUserChats = asyncHandler(async (req: Request, res: Response) => {
+    if (checkValidationError(req, res)) return;
+
+    const user_id = req.query.user_id as string;
+
+    try {
+      if (!(await db.getUserById(user_id))) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      const chats = await db.getAllUserChats(user_id);
+      return res.status(201).json({
+        message: 'Successfully fetched all user chats',
+        allChats: chats,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Failed to get chats',
+        error: (error as Error).message,
+      });
     }
   });
 
@@ -123,14 +188,21 @@ class ChatController {
     try {
       // check if user exists and is inside of chat
       if (!(await db.isUserInsideChat(chat_id, user_id))) {
-        return res.status(403).json({ message: 'Chat ID or User ID not found' });
+        return res
+          .status(403)
+          .json({ message: 'Chat ID or User ID not found' });
       }
 
       const allMessages = await db.getAllChatMessages(chat_id);
 
-      return res.status(201).json({ message: 'Successfully fetched all messages', allMessages });
+      return res
+        .status(201)
+        .json({ message: 'Successfully fetched all messages', allMessages });
     } catch (error) {
-      return res.status(500).json({ message: 'Failed to fetch messages', error: (error as Error).message });
+      return res.status(500).json({
+        message: 'Failed to fetch messages',
+        error: (error as Error).message,
+      });
     }
   });
 }
