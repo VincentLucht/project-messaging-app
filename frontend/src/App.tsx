7f8eaa6356814d24db986a26b18627996997c79e
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-25 14:33:16 +0200

    Update imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import Login from './app/auth/login/Login';
-import { useAuth } from './app/auth/context/hooks/useAuth';
+import Login from '@/app/auth/login/Login';
+import { useAuth } from '@/app/auth/context/hooks/useAuth';
 
-import Home from './app/middle/Home/Home';
-import Chat from './app/right/ActiveChat/Chat';
+import Home from '@/app/middle/Home/Home';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,26 +12,20 @@ import 'react-toastify/dist/ReactToastify.css';
 export const API_URL = 'http://localhost:3005';
 
 export default function App() {
-  const navigate = useNavigate();
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, logout } = useAuth();
 
   useEffect(() => {
     if (!isLoggedIn) {
-      navigate('/login');
+      logout();
     }
-  }, [isLoggedIn, navigate]);
+  }, [isLoggedIn, logout]);
 
   return (
     <div>
-      <main>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/test" element={<Chat />} />
-        </Routes>
-      </main>
-
-      <footer></footer>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
 
       <ToastContainer theme="dark" position="top-center" />
     </div>
