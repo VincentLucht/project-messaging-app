ac624dba55a9c465505111b8df56aeb6545c46b2
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-25 14:36:01 +0200

    Fix interface being wrong

--- frontend/src/app/middle/AllChatsList/api/fetchAllUserChats.ts
+++ frontend/src/app/middle/AllChatsList/api/fetchAllUserChats.ts
@@ -1,19 +1,31 @@
 import { API_URL } from '../../../../App';
 import { DBChat } from '../../../interfaces/databaseSchema';
 
+export interface DBChatWithMembers extends DBChat {
+  UserChats: {
+    user: {
+      id: string;
+      username: string;
+    };
+  };
+}
+
 export interface AllUserChatsResponse {
   message: string;
-  allChats: DBChat[];
+  allChats: DBChatWithMembers[];
 }
 
 export default async function fetchAllUserChats(userId: string, token: string) {
-  const response = await fetch(`${API_URL}/chat?user_id=${userId}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `${token}`,
-      'Content-Type': 'application/json',
+  const response = await fetch(
+    `${API_URL}/chat?user_id=${encodeURIComponent(userId)}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `${token}`,
+        'Content-Type': 'application/json',
+      },
     },
-  });
+  );
 
   if (!response) {
     throw new Error(
