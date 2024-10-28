c9e2fd449569eea50f19a348e07dee10bac7330c
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-28 12:58:51 +0100

    Add styling to messages

--- frontend/src/app/right/ActiveChat/components/ChatMessage.tsx
+++ frontend/src/app/right/ActiveChat/components/ChatMessage.tsx
@@ -14,12 +14,17 @@ export default function ChatMessage({
     <div
       className={`flex ${isCurrentUser ? 'justify-end' : 'justify-start'} my-2`}
     >
-      <div className="flex w-fit max-w-[60%] gap-2 rounded border p-2">
-        <div className="min-w-0 whitespace-pre-wrap break-words">
+      <div
+        className={`flex w-fit max-w-[60%] gap-2 rounded p-2
+          ${isCurrentUser ? 'rounded-br-none bg-blue-500' : 'rounded-bl-none bg-gray-700'}`}
+      >
+        <div className="min-w-0 cursor-text select-text whitespace-pre-wrap break-words pl-2 text-start">
           {message.content}
         </div>
 
-        <div className="self-end text-xs text-gray-500">
+        <div
+          className={`self-end text-xs ${isCurrentUser ? 'text-blue-200' : 'text-gray-400'}`}
+        >
           {dayjs(message.time_created).format('HH:mm')}
         </div>
       </div>
