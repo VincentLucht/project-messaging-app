6a3e391fd4b2e9f3bffd7469bfb9e0dfddaea269
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-28 13:00:00 +0100

    Add joining chat for only notifications

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -11,11 +11,22 @@ export function setupSocketIO(httpServer: any) {
   io.on('connection', (socket: Socket) => {
     console.log('User connected!');
 
+    // join room room without fetching data
+    socket.on(
+      'joinChatNotifications',
+      (data: { chatId: string; userId: string }) => {
+        const notificationRoom = `${data.chatId}:notifications`;
+        socket.join(notificationRoom);
+        console.log(
+          `User ${data.userId} joined notification room ${data.chatId}`,
+        );
+      },
+    );
+
     socket.on('join-chat', async (chatId: string) => {
       try {
         socket.join(chatId);
         socket.emit('chat-joined', chatId);
-        socket.to(chatId).emit('user-joined', socket.id);
         console.log(`User ${socket.id} joined chat ${chatId}`);
       } catch (error) {
         console.error('Error joining chat:', error);
@@ -25,15 +36,28 @@ export function setupSocketIO(httpServer: any) {
 
     socket.on(
       'send-message',
-      async (data: { chatId: string; userId: string; content: string }) => {
+      async (data: {
+        chatId: string;
+        userId: string;
+        username: string;
+        content: string;
+      }) => {
         try {
-          const { chatId, userId, content } = data;
+          const { chatId, userId, content, username } = data;
 
-          await db.createMessage(userId, chatId, content);
+          const newMessage = await db.createMessage(userId, chatId, content);
+          console.log(newMessage);
 
           // send to other user
-          io.to(chatId).emit('new-message', { userId, content });
-          console.log(`Message sent in chat ${chatId}: ${content}`);
+          io.to(chatId).emit('new-message', { userId, content, username });
+          console.log(
+            `User '${userId}' sent a message sent in chat ${chatId}: ${content}`,
+          );
+
+          // send message as notification to other user
+          io.to(`${chatId}:notifications`).emit('newMessageNotification', {
+            newMessage,
+          });
         } catch (error) {
           console.error('Error sending message:', error);
           socket.emit('error', 'Failed to send message');
