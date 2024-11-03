6fab69936bf4a0d2a4a1ade501552b274de1b1c9
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-03 18:09:49 +0100

    Add styling

--- frontend/src/app/left/OpenChatsButton.tsx
+++ frontend/src/app/left/OpenChatsButton.tsx
@@ -1,11 +1,20 @@
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function OpenChatsButton() {
   const navigate = useNavigate();
+  const url = useLocation();
+
+  let imgPath;
+  if (url.pathname === '/chats') {
+    imgPath = './chatIcon.svg';
+  } else {
+    imgPath = './chatIconOutline.svg';
+  }
 
   return (
-    <button className="border" onClick={() => navigate('/chats')}>
-      Click me to go the Chats!
+    <button onClick={() => navigate('/chats')}>
+      <img src={imgPath} alt="Go to chats" />
     </button>
   );
 }
