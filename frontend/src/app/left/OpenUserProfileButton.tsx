027514105064cfc838b706f7248f53cea7113cde
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-03 18:08:49 +0100

    Add profile picture to sidebar

--- frontend/src/app/left/OpenUserProfileButton.tsx
+++ frontend/src/app/left/OpenUserProfileButton.tsx
@@ -1,11 +1,27 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-export default function OpenUserProfileButton() {
+interface OpenUserProfileButtonProps {
+  imgUrl: string | null | undefined;
+}
+
+export default function OpenUserProfileButton({
+  imgUrl,
+}: OpenUserProfileButtonProps) {
   const navigate = useNavigate();
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   return (
-    <button className="border" onClick={() => navigate('/user')}>
-      Click me to go the User page
+    <button onClick={() => navigate('/user')}>
+      <div className="relative h-8 w-8 rounded-full bg-gray-200">
+        <img
+          src={imgUrl ?? './placeholderPFB.jpg'}
+          alt="Go to user profile"
+          className={`absolute left-0 top-0 h-8 w-8 rounded-full object-cover transition-opacity
+            duration-300 ease-in ${imageLoaded ? 'opacity-100' : 'opacity-0'}`}
+          onLoad={() => setImageLoaded(true)}
+        />
+      </div>
     </button>
   );
 }
