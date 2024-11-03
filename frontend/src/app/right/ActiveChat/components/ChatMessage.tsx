07cdd8261fc2bad9b0f9c19f9b25bb249d3f8468
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-03 18:08:37 +0100

    Add styling and placeholder imgs

--- frontend/src/app/right/ActiveChat/components/ChatMessage.tsx
+++ frontend/src/app/right/ActiveChat/components/ChatMessage.tsx
@@ -10,22 +10,42 @@ export default function ChatMessage({
   message,
   isCurrentUser,
 }: ChatMessageProps) {
+  // Determine double tick path
+  const tickPath = './doubleTickSent.svg';
+  const altMessage = 'message sent';
+
   return (
-    <div
-      className={`flex ${isCurrentUser ? 'justify-end' : 'justify-start'} my-2`}
-    >
+    <div className={`flex ${isCurrentUser ? 'justify-end' : 'justify-start'}`}>
       <div
         className={`flex w-fit max-w-[60%] gap-2 rounded p-2
           ${isCurrentUser ? 'rounded-br-none bg-blue-500' : 'rounded-bl-none bg-gray-700'}`}
       >
-        <div className="min-w-0 cursor-text select-text whitespace-pre-wrap break-words pl-2 text-start">
+        {/* message text content */}
+        <div
+          className="min-w-0 cursor-text select-text whitespace-pre-wrap break-words pl-2 text-start
+            text-white"
+        >
           {message.content}
         </div>
 
-        <div
-          className={`self-end text-xs ${isCurrentUser ? 'text-blue-200' : 'text-gray-400'}`}
-        >
-          {dayjs(message.time_created).format('HH:mm')}
+        <div className="flex items-end gap-1">
+          {/* message time_created */}
+          <div
+            className={`self-end text-xs ${isCurrentUser ? 'text-blue-200' : 'text-gray-400'}`}
+          >
+            {dayjs(message.time_created).format('HH:mm')}
+          </div>
+
+          {/* message status */}
+          {isCurrentUser && (
+            <div className="h-auto w-5">
+              <img
+                src={tickPath}
+                alt={altMessage}
+                className="translate-y-[2px]"
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
