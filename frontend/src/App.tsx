0a7091ef3508f6631db0457fa9aceda99338bc3c
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-15 00:56:46 +0100

    Change toast location to top right for less intrusion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,7 +27,7 @@ export default function App() {
         <Route path="/*" element={<Home />} />
       </Routes>
 
-      <ToastContainer theme="dark" position="top-center" />
+      <ToastContainer theme="dark" position="top-right" />
     </div>
   );
 }
