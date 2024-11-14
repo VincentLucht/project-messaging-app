b696401cf125bbf139347d6f1fe4124f8d52d0e7
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-15 00:57:37 +0100

    Lazy load user PFP

--- frontend/src/app/left/OpenUserProfileButton.tsx
+++ frontend/src/app/left/OpenUserProfileButton.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import LazyLoadImage from '@/app/components/LazyLoadImage';
 
 interface OpenUserProfileButtonProps {
   imgUrl: string | null | undefined;
@@ -9,19 +9,14 @@ export default function OpenUserProfileButton({
   imgUrl,
 }: OpenUserProfileButtonProps) {
   const navigate = useNavigate();
-  const [imageLoaded, setImageLoaded] = useState(false);
 
   return (
     <button onClick={() => navigate('/user')}>
-      <div className="relative h-8 w-8 rounded-full bg-gray-200">
-        <img
-          src={imgUrl ?? './placeholderPFB.jpg'}
-          alt="Go to user profile"
-          className={`absolute left-0 top-0 h-8 w-8 rounded-full object-cover transition-opacity
-            duration-300 ease-in ${imageLoaded ? 'opacity-100' : 'opacity-0'}`}
-          onLoad={() => setImageLoaded(true)}
-        />
-      </div>
+      <LazyLoadImage
+        src={imgUrl ?? './placeholderPFP.jpg'}
+        alt="Go to User profile"
+        className="left-0 top-0 h-8 w-8 rounded-full object-cover"
+      />
     </button>
   );
 }
