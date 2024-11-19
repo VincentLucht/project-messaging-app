61cf9e6975d5e2c289957f3d7d01b7b516439589
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-19 18:17:08 +0100

    Add close button util component

---
+++ frontend/src/app/components/CloseButton.tsx
@@ -0,0 +1,22 @@
+import { Dispatch, SetStateAction } from 'react';
+
+interface CloseButtonProps {
+  className?: string;
+  setterFunction: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function CloseButton({
+  className,
+  setterFunction,
+}: CloseButtonProps) {
+  return (
+    <button
+      className={`cursor-pointer text-2xl font-medium transition-all hover:scale-110
+        hover:text-red-600 active:scale-90 ${className}`}
+      onClick={() => setterFunction(false)}
+      aria-label="Close Group Info"
+    >
+      &times;
+    </button>
+  );
+}
