b130896096af5030c24a923228584db1adc19849
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-19 18:34:01 +0100

    Separate into own file, that fetches all chats and unread messages

---
+++ frontend/src/app/middle/Home/services/fetchChats/fetchChats.ts
@@ -0,0 +1,43 @@
+import { Socket } from 'socket.io-client';
+import { DBChatWithMembers } from '@/app/middle/AllChatsList/api/fetchAllUserChats';
+import { User } from '@/app/middle/Home/Home';
+
+import fetchAllUserChats from '@/app/middle/AllChatsList/api/fetchAllUserChats';
+import addUnreadMessagesToChat from '@/app/middle/Home/services/fetchChats/util/addUnreadMessagesToChat';
+import { UnreadMessage } from '@/app/middle/Home/services/fetchChats/util/addUnreadMessagesToChat';
+import { toast } from 'react-toastify';
+
+export default async function fetchChats(
+  isLoggedIn: boolean,
+  user: User | null,
+  token: string | null,
+  socket: Socket | null,
+  logout: () => void,
+  setChats: (chats: DBChatWithMembers[]) => void,
+) {
+  if (isLoggedIn && user && token) {
+    try {
+      const fetchedChats = await fetchAllUserChats(user.id, token);
+
+      if (fetchedChats.allChats === undefined) {
+        logout();
+        return;
+      }
+
+      socket?.emit('getUnreadMessages', {
+        chatIds: fetchedChats.allChats.map((chat) => chat.id),
+        userId: user.id,
+      });
+
+      socket?.on('receiveUnreadMessages', (unreadMessages: UnreadMessage[]) => {
+        const newChats = addUnreadMessagesToChat(
+          fetchedChats.allChats,
+          unreadMessages,
+        );
+        setChats(newChats);
+      });
+    } catch (error) {
+      toast.error(`${(error as Error).message}`);
+    }
+  }
+}
