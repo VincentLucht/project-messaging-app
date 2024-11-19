94452f3d74180dd990d916c026d25040f256fd95
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-19 18:16:03 +0100

    Add typing util class for better code readability

---
+++ backend/src/server/typingUsers/typingUsers.ts
@@ -0,0 +1,44 @@
+import { Socket } from 'socket.io';
+
+export interface TypingUsersInterface {
+  [key: string]: any;
+}
+
+class TypingUsers {
+  private chats: TypingUsersInterface;
+  constructor() {
+    this.chats = {};
+  }
+
+  getChats() {
+    return this.chats;
+  }
+
+  addUsername(chatId: string, username: string) {
+    this.chats[chatId] = { ...this.chats[chatId], [username]: true };
+  }
+
+  deleteUsername(chatId: string, username: string) {
+    if (this.chats[chatId] && this.chats[chatId][username]) {
+      delete this.chats[chatId][username];
+    }
+  }
+
+  clearTypingStatus(chatId: string, username: string) {
+    if (this.chats[chatId]) {
+      delete this.chats[chatId][username];
+      if (Object.keys(this.chats[chatId]).length === 0) {
+        delete this.chats[chatId];
+      }
+    }
+  }
+
+  emit(chatId: string, socket: Socket) {
+    socket
+      .to(`${chatId}:notifications`)
+      .emit('typing-users', typingUsers.getChats(), chatId);
+  }
+}
+
+const typingUsers = new TypingUsers();
+export default typingUsers;
