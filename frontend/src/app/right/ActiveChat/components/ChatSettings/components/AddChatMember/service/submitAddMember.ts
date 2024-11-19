83a52e8e609cbd2a4fecb3331f9b90da11e15d62
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-19 18:41:58 +0100

    Add service function when adding a chat member

---
+++ frontend/src/app/right/ActiveChat/components/ChatSettings/components/AddChatMember/service/submitAddMember.ts
@@ -0,0 +1,63 @@
+import {
+  ExpressError,
+  ExpressErrors,
+} from '@/app/interfaces/express-validator-errors';
+import addChatMember from '@/app/right/ActiveChat/components/ChatSettings/components/AddChatMember/api/addChatMember';
+import { toast } from 'react-toastify';
+import toastUpdateOptions from '@/app/components/ts/toastUpdateObject';
+import { Socket } from 'socket.io-client';
+
+export default function submitAddMember(
+  userId: string,
+  username: string,
+  otherUsername: string,
+  chatId: string,
+  token: string,
+  socket: Socket | null,
+) {
+  const toastId = toast.loading('Adding user to chat...');
+
+  if (!otherUsername) {
+    toast.update(
+      toastId,
+      toastUpdateOptions(
+        'Please add at least one character to the username',
+        'warning',
+      ),
+    );
+    return;
+  }
+
+  addChatMember(username, otherUsername, chatId, token)
+    .then((response) => {
+      toast.update(
+        toastId,
+        toastUpdateOptions('Added user to chat', 'success'),
+      );
+
+      // Send signal to server that a user was added to chat
+      socket?.emit(
+        'user-added-to-chat',
+        chatId,
+        userId,
+        username,
+        response.newUser,
+      );
+    })
+    .catch((error: ExpressErrors) => {
+      // check for express errors
+      if ('errors' in error && Array.isArray(error.errors)) {
+        error.errors.forEach((err: ExpressError) => {
+          toast.error(err.msg);
+        });
+        // generic error
+      } else if (error.message) {
+        toast.error(`${error.message}`, { autoClose: 10000 });
+      }
+
+      toast.update(
+        toastId,
+        toastUpdateOptions('Failed to add user to chat', 'error'),
+      );
+    });
+}
