a7bc4514728231c6eb35431305dd30015453deb6
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-11-27 17:12:38 +0100

    Add service func that handle various chat changes

--- frontend/src/app/middle/Home/services/handleChatChanges.ts
+++ frontend/src/app/middle/Home/services/handleChatChanges.ts
@@ -1,34 +1,37 @@
+import { Dispatch, SetStateAction } from 'react';
+import { Socket } from 'socket.io-client';
+
 import { DBMessage } from '@/app/interfaces/databaseSchema';
 import { DBChatWithMembers } from '@/app/middle/AllChatsList/api/fetchAllUserChats';
-import { Socket } from 'socket.io-client';
 import { User } from '@/app/middle/Home/Home';
 
-export default function handleNotifications(
+export default function handleChatChanges(
   chats: DBChatWithMembers[] | null,
   setChats: (chats: DBChatWithMembers[]) => void,
   activeChat: DBChatWithMembers | null,
+  setActiveChat: Dispatch<SetStateAction<DBChatWithMembers | null>>,
   user: User | null,
   socket: Socket | null,
 ) {
   if (!chats || !socket) return;
 
-  socket.on('newMessageNotification', (data: { newMessage: DBMessage }) => {
-    const { newMessage } = data;
+  socket.on('newMessageNotification', (data: { sentMessage: DBMessage }) => {
+    const { sentMessage } = data;
 
     // update chat order
     let updatedChat;
     const filteredChat: DBChatWithMembers[] = [];
     chats.forEach((chat) => {
-      if (chat.id === newMessage.chat_id) {
+      if (chat.id === sentMessage.chat_id) {
         updatedChat = {
           ...chat,
-          last_message: newMessage,
-          last_message_id: newMessage.id,
+          last_message: sentMessage,
+          last_message_id: sentMessage.id,
           time_updated: new Date().toISOString(),
           unreadCount:
             // ? only increase if not from user AND user not inside of chat
-            newMessage.user_id === user?.id ||
-            newMessage.chat_id === activeChat?.id
+            sentMessage.user_id === user?.id ||
+            sentMessage.chat_id === activeChat?.id
               ? chat.unreadCount
               : chat.unreadCount + 1,
         };
@@ -65,9 +68,37 @@ export default function handleNotifications(
 
       if (updatedChat) {
         filteredChat.unshift(updatedChat);
+        if (activeChat) {
+          setActiveChat(updatedChat);
+        }
       }
 
       setChats(filteredChat);
+    },
+  );
+
+  socket.on(
+    'chat-description-changed',
+    (data: { chatId: string; newChatDescription: string }) => {
+      const { chatId, newChatDescription } = data;
+
+      const newChats = chats.map((chat) => {
+        if (chat.id === chatId) {
+          return {
+            ...chat,
+            chat_description: newChatDescription,
+          };
+        } else {
+          return chat;
+        }
+      });
+
+      if (activeChat) {
+        const newChat = newChats.find((chat) => chat.id === chatId) ?? null;
+        setActiveChat(newChat);
+      }
+
+      setChats(newChats);
     },
   );
 }
