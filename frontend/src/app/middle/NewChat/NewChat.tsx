f1662bb40546ee29b078a3ca1917dc54f252b30f
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-02 13:28:46 +0100

    Complete component, 100% functionality now:
    - can create one-on-one chat or group chats
    => can add multiple users at the same time
    - good error handling for incorrect user names and adding yourself

--- frontend/src/app/middle/NewChat/NewChat.tsx
+++ frontend/src/app/middle/NewChat/NewChat.tsx
@@ -1,63 +1,100 @@
-import { FormEvent, useState } from 'react';
-import { useAuth } from '../../auth/context/hooks/useAuth';
+import { FormEvent, useState, Dispatch, SetStateAction } from 'react';
+import { useAuth } from '@/app/auth/context/hooks/useAuth';
 
 import { toast } from 'react-toastify';
-import toastUpdateOptions from '../../components/ts/toastUpdateObject';
+import toastUpdateOptions from '@/app/components/ts/toastUpdateObject';
 
-import handleCreateChat from './api/handleCreateChat';
+import handleCreateChat from '@/app/middle/NewChat/api/handleCreateChat';
+import convertUsernames from '@/app/middle/NewChat/util/convertUsernames';
 
-import Input from '../../components/Input';
-import { InputWithErrors } from '../../components/InputWithErrors';
-import { ValidationError } from '../../auth/login/Login';
-import ShowValidationErrors from '../../components/ShowValidationError';
-import convertExpressErrors from '../../components/ts/convertExpressErrors';
+import Input from '@/app/components/Input';
+import Checkbox from '@/app/components/Checkbox';
 
-import Checkbox from '../../components/Checkbox';
-import NotLoggedIn from '../../components/partials/NotLoggedIn';
+import './css/NewChat.css';
+import { DBChatWithMembers } from '@/app/middle/AllChatsList/api/fetchAllUserChats';
+import { Socket } from 'socket.io-client';
+import generateTempId from '@/app/right/ActiveChat/util/generateTempId';
 
 interface NewChatProps {
-  onChatCreated: () => void;
+  socket: Socket | null;
+  setChats: Dispatch<SetStateAction<DBChatWithMembers[] | null>>;
 }
 
-export default function NewChat({
-  onChatCreated: refreshAllChatsLists,
-}: NewChatProps) {
-  const [chatName, setChatName] = useState('');
+export default function NewChat({ socket, setChats }: NewChatProps) {
   const [isGroupChat, setIsGroupChat] = useState(false);
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+  const [otherUsernames, setOtherUsernames] = useState('');
+  const [profilePictureUrl, setProfilePictureUrl] = useState('');
+  const [chatName, setChatName] = useState('');
+  const [chatDescription, setChatDescription] = useState('');
 
+  const [errors, setErrors] = useState('');
+  const [incorrectUsers, setIncorrectUsers] = useState<string[]>([]);
   const { user, token } = useAuth();
 
-  const onSubmit = async (e: FormEvent) => {
+  // ! TODO: Put into different file??
+  const onSubmit = (e: FormEvent) => {
     e.preventDefault();
-    const toastId = toast.loading('Creating Chat...');
-
     if (!user || !token) {
-      return <NotLoggedIn />;
+      return;
     }
 
-    try {
-      await handleCreateChat(user.id, token, chatName, isGroupChat, password);
-      toast.update(
-        toastId,
-        toastUpdateOptions('Successfully created Chat', 'success'),
-      );
+    const toastId = toast.loading('Creating Chat...');
+    handleCreateChat(
+      user?.id,
+      token,
+      isGroupChat,
+      convertUsernames(otherUsernames, isGroupChat),
+      chatName,
+      profilePictureUrl,
+      chatDescription,
+    )
+      .then((response) => {
+        if (response.newChat) {
+          // Add unread count
+          const { newChat } = response;
 
-      // reset everything
-      setChatName('');
-      setIsGroupChat(false);
-      setPassword('');
-      setErrors({});
-      refreshAllChatsLists();
-    } catch (error) {
-      const validationErrors = error as { errors: ValidationError[] };
-      setErrors(convertExpressErrors(validationErrors.errors));
-      toast.update(
-        toastId,
-        toastUpdateOptions('Chat Creation failed', 'error'),
-      );
-    }
+          setChats((prevChats) => [
+            {
+              ...newChat,
+              unreadCount: 0,
+              last_message: {
+                chat_id: generateTempId(),
+                content: 'created the Chat',
+                id: generateTempId(),
+                is_system_message: true,
+                time_created: new Date().toISOString(),
+                user: { username: user.username },
+                user_id: user.id,
+              },
+            },
+            ...(prevChats ?? []),
+          ]);
+
+          // Send signal to backend
+          socket?.emit('create-new-chat', user.id, user.username, newChat);
+        }
+
+        setIncorrectUsers([]);
+        setErrors('');
+        toast.update(
+          toastId,
+          toastUpdateOptions('Successfully created new Chat', 'success'),
+        );
+      })
+      .catch((error: { message: string; incorrectUsers?: string[] }) => {
+        setIncorrectUsers([]);
+        setErrors('');
+
+        if (error.incorrectUsers) {
+          setIncorrectUsers(error.incorrectUsers);
+        } else {
+          setErrors(error.message);
+        }
+        toast.update(
+          toastId,
+          toastUpdateOptions('Failed to create the Chat', 'error'),
+        );
+      });
   };
 
   return (
@@ -68,33 +105,92 @@ export default function NewChat({
       >
         <h2 className="text-2xl font-bold">Create New Chat</h2>
 
-        {/* Room name */}
-        <InputWithErrors
-          value={chatName}
-          setValue={setChatName}
-          placeholder="Name"
-          errors={errors}
-        />
-        <ShowValidationErrors className="-mt-2" error={errors.name} />
+        {/* Display errors */}
+        {errors && (
+          <div className="dark-bg gap-2 rounded-md p-2 df">
+            <img className="h-7" src="./alert.svg" alt="alert icon" />
+            <div className="text-lg font-bold text-red-600">
+              You can&apos;t add yourself
+            </div>
+          </div>
+        )}
+        {incorrectUsers.length > 0 && (
+          <div
+            className="max-h-[300px] w-full overflow-y-auto rounded-lg border-2 border-red-600
+              bg-red-50 p-4"
+          >
+            <div className="mb-3 flex items-center gap-1 text-red-600">
+              <img className="h-7" src="./alert.svg" alt="alert icon" />
+              <h3 className="text-lg font-semibold">Usernames Not Found:</h3>
+            </div>
+            <div className="space-y-2">
+              {incorrectUsers.map((incorrectUser, index) => (
+                <div
+                  key={index}
+                  className="rounded-md bg-red-200 px-3 py-2 text-sm text-red-700"
+                >
+                  {incorrectUser}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
 
         {/* is group chat */}
         <Checkbox
-          labelValue="Group Chat*"
+          labelValue="Group Chat*:"
           accessibilityValue="is-group-chat"
           value={isGroupChat}
           setValue={setIsGroupChat}
         />
 
-        {/* password */}
+        {/* Other Usernames */}
         <Input
-          labelValue="Password:"
-          accessibilityValue="password"
-          isRequired={false}
-          value={password}
-          setValue={setPassword}
+          labelValue={`${isGroupChat ? 'Other Usernames (separate with commas)*:' : 'Other Username (separate with commas)*:'}`}
+          accessibilityValue={`${isGroupChat ? 'Other Usernames' : 'Other Username'}`}
+          value={otherUsernames}
+          setValue={setOtherUsernames}
+          isRequired={true}
+          classNameWrapper="new-chat-input-wrapper"
+          className="new-chat-input df"
         />
 
-        {/* TODO: add chat description */}
+        {isGroupChat && (
+          <>
+            {/* Chat name */}
+            <Input
+              labelValue="Chat Name:*"
+              accessibilityValue="Chat name"
+              value={chatName}
+              setValue={setChatName}
+              isRequired={true}
+              classNameWrapper="new-chat-input-wrapper"
+              className="new-chat-input df"
+            />
+
+            {/* PFP URL */}
+            <Input
+              labelValue="Profile Picture URL:"
+              accessibilityValue="Group Profile Picture"
+              value={profilePictureUrl}
+              setValue={setProfilePictureUrl}
+              isRequired={false}
+              classNameWrapper="new-chat-input-wrapper"
+              className="new-chat-input df"
+            />
+
+            {/* Chat Description */}
+            <Input
+              labelValue="Chat Description:"
+              accessibilityValue="Chat Description"
+              value={chatDescription}
+              setValue={setChatDescription}
+              isRequired={false}
+              classNameWrapper="new-chat-input-wrapper"
+              className="new-chat-input df"
+            />
+          </>
+        )}
 
         {/* Submit */}
         <button
