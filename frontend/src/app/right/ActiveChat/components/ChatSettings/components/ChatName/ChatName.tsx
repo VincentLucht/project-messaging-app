110a6bb4e55333cda799c579bb8da11763343798
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-02 13:34:27 +0100

    Fix chat name not being refreshed when it was changed

--- frontend/src/app/right/ActiveChat/components/ChatSettings/components/ChatName/ChatName.tsx
+++ frontend/src/app/right/ActiveChat/components/ChatSettings/components/ChatName/ChatName.tsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
-import EditChatNameButton from '@/app/right/ActiveChat/components/ChatSettings/components/ChatName/components/EditChatNameButton';
-import editGroupChatName from '@/app/right/ActiveChat/components/ChatSettings/components/ChatName/components/api/editGroupChatName';
+import EditButton from '@/app/components/EditButton';
+import editGroupChatName from '@/app/right/ActiveChat/components/ChatSettings/components/ChatName/api/editGroupChatName';
 
 import { toast } from 'react-toastify';
 import toastUpdateOptions from '@/app/components/ts/toastUpdateObject';
@@ -24,12 +24,16 @@ export default function ChatName({
   token,
   socket,
 }: ChatNameProps) {
-  const [isEditActive, setIsEditActive] = useState(false);
   const [newChatName, setNewChatName] = useState(chatName);
-  const [changeChatName, setChangeChatName] = useState(false);
-  const [displayNewChatName, setDisplayNewChatName] = useState('');
-
   const input = useRef<HTMLTextAreaElement>(null);
+  const [isEditActive, setIsEditActive] = useState(false);
+  const [changeChatName, setChangeChatName] = useState(false);
+
+  // Refresh on chat change
+  useEffect(() => {
+    setIsEditActive(false);
+    setChangeChatName(false);
+  }, [chatId]);
 
   // Autofocus the change group name input
   useEffect(() => {
@@ -50,18 +54,16 @@ export default function ChatName({
           .then(() => {
             toast.update(
               toastId,
-              toastUpdateOptions('Successfully changed Chat name', 'success'),
+              toastUpdateOptions('Successfully changed Chat Name', 'success'),
             );
 
             // send signal that chat name change
             socket?.emit('change-chat-name', chatId, newChatName);
-            setDisplayNewChatName(newChatName);
-            // ? Also send message inside of chat?
           })
           .catch(() => {
             toast.update(
               toastId,
-              toastUpdateOptions('Failed to change Chat name', 'error'),
+              toastUpdateOptions('Failed to change Chat Name', 'error'),
             );
           });
       } catch (error) {
@@ -78,40 +80,47 @@ export default function ChatName({
     }
   }, [changeChatName, chatId, newChatName, token, userId, socket]);
 
+  const handleSubmit = () => {
+    if (newChatName !== chatName) {
+      setIsEditActive(false);
+      setChangeChatName(true);
+    } else {
+      toast.warning('You did not change the chat name');
+    }
+  };
+
   return (
     <div className={`flex justify-center gap-2 ${isUserAdmin && '-mr-[28px]'}`}>
       {/* Change group name input */}
       {isEditActive ? (
-        <div className="relative">
-          <TextareaAutosize
-            value={newChatName}
-            onChange={(e) => {
-              if (!newChatName.length) {
-                toast.error("You can't set a chat name to nothing!");
-                return;
-              }
-              setNewChatName(e.target.value);
-            }}
-            className="h-8 w-full min-w-32 max-w-[300px] resize-none rounded-lg text-center text-2xl
-              font-bold focus:border-blue-800"
-            ref={input}
-            style={{ width: `${newChatName.length}ch` }}
-            maxLength={100}
-            spellCheck={false}
-          />
-        </div>
+        <TextareaAutosize
+          value={newChatName}
+          onChange={(e) => setNewChatName(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              handleSubmit();
+            }
+          }}
+          className="h-8 w-full min-w-32 max-w-[300px] resize-none rounded-lg text-center text-2xl
+            font-bold outline-none focus:ring-2 focus:ring-blue-400"
+          ref={input}
+          style={{ width: `${newChatName.length}ch` }}
+          maxLength={100}
+          spellCheck={false}
+        />
       ) : (
-        <h3 className="text-2xl font-bold">
-          {displayNewChatName ? displayNewChatName : chatName}
-        </h3>
+        <h3 className="text-2xl font-bold">{chatName}</h3>
       )}
 
       {/* Activate Edit mode => Edit chat name  */}
-      <EditChatNameButton
+      <EditButton
         isUserAdmin={isUserAdmin}
         isEditActive={isEditActive}
         setIsEditActive={setIsEditActive}
-        setChangeChatName={setChangeChatName}
+        confirmSetterFunc={setChangeChatName}
+        handleSubmit={handleSubmit}
+        isDisabled={newChatName.length === 0}
       />
     </div>
   );
