4bd51cdbb1c8e9313d2c5f937cec5ae1c2ee6444
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-02 13:15:20 +0100

    Add extra state to show loading error

--- frontend/src/app/components/LazyLoadImage.tsx
+++ frontend/src/app/components/LazyLoadImage.tsx
@@ -8,17 +8,27 @@ interface LazyLoadImageProps {
 
 const LazyLoadImage = ({ src, alt, className = '' }: LazyLoadImageProps) => {
   const [imageLoaded, setImageLoaded] = useState(false);
+  const [imageError, setImageError] = useState(false);
 
   return (
-    <img
-      src={src ?? './placeholderPFP.jpg'}
-      alt={!src ? 'Picture unable to load' : alt}
-      className={`${className} transition-opacity duration-300 ease-in ${
-        imageLoaded ? 'opacity-100' : 'opacity-0'}`}
-      loading="lazy"
-      decoding="async"
-      onLoad={() => setImageLoaded(true)}
-    />
+    <>
+      {imageError ? (
+        <div className={`${className} text-center text-sm df`}>
+          Picture unable to load
+        </div>
+      ) : (
+        <img
+          src={src ?? './placeholderPFP.jpg'}
+          alt={!src ? 'Picture unable to load' : alt}
+          className={`${className} transition-opacity duration-300 ease-in ${
+            imageLoaded ? 'opacity-100' : 'opacity-0'}`}
+          loading="lazy"
+          decoding="async"
+          onLoad={() => setImageLoaded(true)}
+          onError={() => setImageError(true)}
+        />
+      )}
+    </>
   );
 };
 
