95dd4d39157e44c248a6bc966d43dcab729f7b84
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-07 10:43:56 +0100

    Fix incorrect state update

--- frontend/src/app/middle/Home/services/handleUserAddedToChat.ts
+++ frontend/src/app/middle/Home/services/handleUserAddedToChat.ts
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from 'react';
 import { DBUser } from '@/app/interfaces/databaseSchema';
 import { DBChatWithMembers } from '@/app/middle/AllChatsList/api/fetchAllUserChats';
 import generateTempId from '@/app/right/ActiveChat/util/generateTempId';
@@ -6,7 +7,9 @@ import { Socket } from 'socket.io-client';
 export default function handleUserAddedToChat(
   socket: React.RefObject<Socket> | null,
   chats: DBChatWithMembers[] | null,
-  setChats: (chats: DBChatWithMembers[]) => void,
+  setChats: Dispatch<SetStateAction<DBChatWithMembers[] | null>>,
+  activeChat: DBChatWithMembers | null,
+  setActiveChat: Dispatch<SetStateAction<DBChatWithMembers | null>>,
 ) {
   if (!socket || !chats) return;
 
@@ -15,33 +18,58 @@ export default function handleUserAddedToChat(
     (data: { chatId: string; newUser: DBUser }) => {
       const { chatId, newUser } = data;
 
-      // add user to chat and update chat order
-      const newChats = chats.map((chat) => {
-        if (chat.id === chatId) {
+      setChats((prevChats) => {
+        if (!prevChats) return prevChats;
+
+        const updatedChats = prevChats.map((chat) => {
+          if (chat.id === chatId) {
+            return {
+              ...chat,
+              UserChats: [
+                ...chat.UserChats,
+                {
+                  id: generateTempId(),
+                  user_id: newUser.id,
+                  chat_id: chatId,
+                  user: {
+                    id: newUser.id,
+                    name: newUser.name,
+                    username: newUser.username,
+                    profile_picture_url:
+                      newUser.profile_picture_url ?? undefined,
+                    user_description: newUser.user_description,
+                  },
+                },
+              ],
+            };
+          }
+          return chat;
+        });
+
+        return updatedChats;
+      });
+
+      if (activeChat?.id === chatId) {
+        setActiveChat((prev) => {
+          if (!prev) return null;
+
           return {
-            ...chat,
+            ...prev,
             UserChats: [
-              ...chat.UserChats,
+              ...prev.UserChats,
               {
-                id: generateTempId(),
-                user_id: newUser.id,
-                chat_id: chatId,
                 user: {
                   id: newUser.id,
                   name: newUser.name,
                   username: newUser.username,
-                  profile_picture_url: newUser.profile_picture_url ?? undefined,
                   user_description: newUser.user_description,
+                  profile_picture_url: newUser.profile_picture_url ?? undefined,
                 },
               },
             ],
           };
-        } else {
-          return chat;
-        }
-      });
-
-      setChats(newChats);
+        });
+      }
     },
   );
 }
