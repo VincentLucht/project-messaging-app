c19ff5e4e5283d848bb0bffeaa3bb57de046e8b5
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-07 10:41:49 +0100

    Add support for tracking multiple devices

--- backend/src/server/controllers/Tracker.ts
+++ backend/src/server/controllers/Tracker.ts
@@ -3,19 +3,21 @@ class Tracker {
     socketId: string,
     username: string,
     onlineUsers: Map<string, Set<string>>,
-    socketToUser: Map<string, string>,
+    socketToUser: Map<string, Set<string>>,
   ) {
     if (!onlineUsers.has(username)) {
       const userSet: Set<string> = new Set();
       userSet.add(socketId);
       onlineUsers.set(username, userSet);
-
-      // reverse track username and connections
-      socketToUser.set(socketId, username);
     } else {
-      // track multiple user connections
       onlineUsers.get(username)?.add(socketId);
     }
+
+    // Manage user connecting on multiple devices
+    if (!socketToUser.has(socketId)) {
+      socketToUser.set(socketId, new Set());
+    }
+    socketToUser.get(socketId)?.add(username);
 
     console.log({ onlineUsers, socketToUser });
   }
@@ -23,12 +25,22 @@ class Tracker {
   deleteOnlineUsers(
     socketId: string,
     onlineUsers: Map<string, Set<string>>,
-    socketToUser: Map<string, string>,
+    socketToUser: Map<string, Set<string>>,
   ) {
-    // check if the user is online via socket id
-    const username = socketToUser.get(socketId);
-    if (username) {
-      onlineUsers.delete(username);
+    const usernames = socketToUser.get(socketId);
+    if (usernames) {
+      usernames.forEach((username) => {
+        const userSockets = onlineUsers.get(username);
+        if (userSockets) {
+          userSockets.delete(socketId);
+          // Remove username entry if no sockets remain
+          if (userSockets.size === 0) {
+            onlineUsers.delete(username);
+          }
+        }
+      });
+
+      // Clean up the socket ID entry
       socketToUser.delete(socketId);
     }
 
