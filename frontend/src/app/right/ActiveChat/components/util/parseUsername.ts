7897e090614ea17d98bb0163a85f37fa4a0abb04
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-21 10:38:40 +0100

    Rework component to handle all system message types

---
+++ frontend/src/app/right/ActiveChat/components/util/parseUsername.ts
@@ -0,0 +1,22 @@
+export default function parseUsername(content: string, username: string) {
+  // ! TODO: Rename to parseUsername and replace all old calls
+  const message = content.split(' ');
+
+  if (message[0] === 'added') {
+    const foundUsername = message.slice(1, -3).join(' ');
+    return foundUsername === username ? 'added You to the Chat' : content;
+  } else if (message[0] === 'made') {
+    const foundUsername = message.slice(1, -2).join(' ');
+    return foundUsername === username ? 'made You an Admin' : content;
+  } else if (message[0] === 'revoked') {
+    const foundUsername = message.slice(5).join(' ');
+    return foundUsername === username
+      ? 'revoked the Admin Role from You'
+      : content;
+  } else if (message[0] === 'removed') {
+    const foundUsername = message.slice(1, -3).join(' ');
+    return foundUsername === username ? 'removed You from the Chat' : content;
+  } else {
+    return content;
+  }
+}
