b0f8adc2c9efec5d4a3c5ff0ca3414f8797a3ad6
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-22 13:58:56 +0100

    Add message limit

--- frontend/src/app/right/ActiveChat/components/ChatMessageForm/SendMessageForm.tsx
+++ frontend/src/app/right/ActiveChat/components/ChatMessageForm/SendMessageForm.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import { Socket } from 'socket.io-client';
 import { encryptMessage } from '@/app/secure/cryptoUtils';
 import sendEncryptedMessage from '@/app/secure/sendEncryptedMessage';
+import { toast } from 'react-toastify';
 
 interface SendMessageFormProps {
   socket: Socket | null;
@@ -21,6 +22,34 @@ export default function SendMessageForm({
 }: SendMessageFormProps) {
   const [message, setMessage] = useState('');
   const [isTyping, setIsTyping] = useState(false);
+  const input = useRef<HTMLTextAreaElement>(null);
+  const previousChatId = useRef(chatId);
+
+  useEffect(() => {
+    if (previousChatId.current !== chatId) {
+      socket?.emit('stopped-typing', {
+        chatId: previousChatId.current,
+        username,
+      });
+    }
+    previousChatId.current = chatId;
+  }, [chatId, socket, username]);
+
+  useEffect(() => {
+    if (input.current) {
+      input.current.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (message.length >= 7000) {
+        toast.warn('You reached the maximum amount of characters in a message');
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [message]);
 
   const sendMessage = (e: React.FormEvent | string) => {
     if (typeof e !== 'string') {
@@ -78,6 +107,8 @@ export default function SendMessageForm({
           className="flex-1 resize-none rounded-3xl border-2 px-6 py-3"
           placeholder="Enter your message"
           maxRows={6}
+          ref={input}
+          maxLength={7000}
         />
 
         <button
