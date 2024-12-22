3ac4500b516eb446e2c3b17d7ebad397611c147a
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-22 12:45:10 +0100

    Fix typo

--- frontend/src/app/middle/UserProfile/components/ChangeName/ChangeName.tsx
+++ frontend/src/app/middle/UserProfile/components/ChangeName/ChangeName.tsx
@@ -44,7 +44,7 @@ export default function ChangeName({ user, token, logout }: ChangeNameProps) {
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-left font-bold">Your Description:</div>
+      <div className="text-left font-bold">Your Alias:</div>
 
       <div className="flex justify-between">
         {isEditActive ? (
