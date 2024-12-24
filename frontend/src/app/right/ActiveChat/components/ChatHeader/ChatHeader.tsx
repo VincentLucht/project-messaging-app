8f8aa6c98e59af9966deb3b37f530fb6fead5067
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-24 13:23:57 +0100

    Fix buggy animation on typing activity, add button to access settings

--- frontend/src/app/right/ActiveChat/components/ChatHeader/ChatHeader.tsx
+++ frontend/src/app/right/ActiveChat/components/ChatHeader/ChatHeader.tsx
@@ -1,10 +1,11 @@
 import { Dispatch, SetStateAction } from 'react';
 import { TypingUsers } from '@/app/interfaces/TypingUsers';
 
-import DisplayTypingUsers from '@/app/middle/AllChatsList/components/util/DisplayTypingUsers';
+import DisplayTypingUsers from '@/app/middle/Home/components/ChatSection/AllChatsList/components/util/DisplayTypingUsers';
 import { DBMessage } from '@/app/interfaces/databaseSchema';
+import { ChatMember } from '@/app/middle/Home/components/ChatSection/AllChatsList/api/fetchAllUserChats';
+import displayChatName from '@/app/right/ActiveChat/components/ChatSettings/components/ChatName/components/DisplayChatName';
 
-// ! TODO: Fix "Load more" button not sticking to the top
 interface ChatHeaderProps {
   showChatSettings: boolean;
   setShowChatSettings: Dispatch<SetStateAction<boolean>>;
@@ -12,6 +13,8 @@ interface ChatHeaderProps {
   chatId: string;
   chatName: string;
   isGroupChat: boolean;
+  chatMembers: { user: ChatMember }[];
+  userId: string;
   username: string;
   lastChatMessage: DBMessage | null;
 }
@@ -23,6 +26,8 @@ export default function ChatHeader({
   chatId,
   chatName,
   isGroupChat,
+  chatMembers,
+  userId,
   username,
   lastChatMessage,
 }: ChatHeaderProps) {
@@ -37,30 +42,41 @@ export default function ChatHeader({
 
   return (
     <div
-      className="relative max-h-[48px] min-h-[48px] cursor-pointer overflow-hidden"
+      className="flex max-h-[50px] min-h-[50px] w-full cursor-pointer transition-colors
+        hover:bg-gray-strong"
       onClick={() => setShowChatSettings(!showChatSettings)}
     >
-      {/* chat name */}
-      <h2
-        className={`absolute overflow-hidden overflow-ellipsis whitespace-nowrap px-5 py-2 text-left
-          text-2xl font-bold transition-all ${hasTypingUsers &&
-          'translate-y-[-10px] scale-75'}`}
-      >
-        {chatName}
-      </h2>
+      <div className="flex w-full flex-col">
+        {/* chat name */}
+        <h2
+          className={`origin-left overflow-ellipsis whitespace-nowrap px-5 py-2 text-left text-2xl
+            font-bold transition-all ${hasTypingUsers && 'translate-y-[-10px] scale-75'}`}
+        >
+          {displayChatName(chatName, isGroupChat, chatMembers, userId)}
+        </h2>
 
-      {/* activity indicator */}
-      <div
-        className={`absolute pl-10 pt-4 transition-all ${hasTypingUsers && 'translate-y-[6px]'}`}
-      >
-        {hasTypingUsers &&
-          DisplayTypingUsers(
-            typingUsers[chatId],
-            username,
-            lastChatMessage,
-            isGroupChat,
-            'precise',
-          )}
+        {/* activity indicator */}
+        <div
+          className={`absolute pl-4 pt-4 transition-all
+            ${hasTypingUsers ? 'translate-y-[6px] opacity-100' : 'opacity-0'}`}
+        >
+          {hasTypingUsers &&
+            DisplayTypingUsers(
+              typingUsers[chatId],
+              username,
+              lastChatMessage,
+              isGroupChat,
+              'precise',
+            )}
+        </div>
+      </div>
+
+      <div className="mr-4 df">
+        <img
+          className="h-4 min-h-4 w-4 min-w-4 hover:scale-[115%] active:scale-95"
+          src="./kebabMenu.svg"
+          alt="kebab menu for accessing chat settings"
+        />
       </div>
     </div>
   );
