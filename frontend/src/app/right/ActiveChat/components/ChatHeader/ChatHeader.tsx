6d6d2188a39f8ff7174109f0c590728a1201458c
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-12-25 12:30:38 +0100

    Add mobile design

--- frontend/src/app/right/ActiveChat/components/ChatHeader/ChatHeader.tsx
+++ frontend/src/app/right/ActiveChat/components/ChatHeader/ChatHeader.tsx
@@ -3,12 +3,14 @@ import { TypingUsers } from '@/app/interfaces/TypingUsers';
 
 import DisplayTypingUsers from '@/app/middle/Home/components/ChatSection/AllChatsList/components/util/DisplayTypingUsers';
 import { DBMessage } from '@/app/interfaces/databaseSchema';
+import { DBChatWithMembers } from '@/app/middle/Home/components/ChatSection/AllChatsList/api/fetchAllUserChats';
 import { ChatMember } from '@/app/middle/Home/components/ChatSection/AllChatsList/api/fetchAllUserChats';
 import displayChatName from '@/app/right/ActiveChat/components/ChatSettings/components/ChatName/components/DisplayChatName';
 
 interface ChatHeaderProps {
   showChatSettings: boolean;
   setShowChatSettings: Dispatch<SetStateAction<boolean>>;
+  setActiveChat: Dispatch<SetStateAction<DBChatWithMembers | null>>;
   typingUsers: TypingUsers;
   chatId: string;
   chatName: string;
@@ -17,11 +19,13 @@ interface ChatHeaderProps {
   userId: string;
   username: string;
   lastChatMessage: DBMessage | null;
+  isMobile: boolean;
 }
 
 export default function ChatHeader({
   showChatSettings,
   setShowChatSettings,
+  setActiveChat,
   typingUsers,
   chatId,
   chatName,
@@ -30,6 +34,7 @@ export default function ChatHeader({
   userId,
   username,
   lastChatMessage,
+  isMobile,
 }: ChatHeaderProps) {
   const allTypingUsers = typingUsers[chatId]
     ? Object.keys(typingUsers[chatId])
@@ -42,15 +47,29 @@ export default function ChatHeader({
 
   return (
     <div
-      className="flex max-h-[50px] min-h-[50px] w-full cursor-pointer transition-colors
-        hover:bg-gray-strong"
+      className={`grid max-h-[50px] min-h-[50px] w-full cursor-pointer
+        ${isMobile ? 'grid-cols-[auto_1fr_auto]' : 'grid-cols-[1fr_auto]'}
+        transition-colors hover:bg-gray-strong`}
       onClick={() => setShowChatSettings(!showChatSettings)}
     >
-      <div className="flex w-full flex-col">
+      {isMobile && (
+        <button
+          className="ml-3 min-h-[34px] min-w-[34px] transition-all hover:scale-[120%]"
+          onClick={(e) => {
+            e.stopPropagation();
+            setActiveChat(null);
+          }}
+        >
+          <img className="" src="chevron-left.svg" alt="go back icon" />
+        </button>
+      )}
+
+      <div className="flex w-full max-w-[80dvw] flex-col">
         {/* chat name */}
         <h2
-          className={`origin-left overflow-ellipsis whitespace-nowrap px-5 py-2 text-left text-2xl
-            font-bold transition-all ${hasTypingUsers && 'translate-y-[-10px] scale-75'}`}
+          className={`origin-left truncate overflow-ellipsis whitespace-nowrap px-5 py-2 text-left
+            text-2xl font-bold transition-all ${hasTypingUsers &&
+            'translate-y-[-10px] scale-75'}`}
         >
           {displayChatName(chatName, isGroupChat, chatMembers, userId)}
         </h2>
@@ -58,7 +77,7 @@ export default function ChatHeader({
         {/* activity indicator */}
         <div
           className={`absolute pl-4 pt-4 transition-all
-            ${hasTypingUsers ? 'translate-y-[6px] opacity-100' : 'opacity-0'}`}
+            ${hasTypingUsers ? 'translate-y-[8px] opacity-100' : 'opacity-0'}`}
         >
           {hasTypingUsers &&
             DisplayTypingUsers(
@@ -73,7 +92,7 @@ export default function ChatHeader({
 
       <div className="mr-4 df">
         <img
-          className="h-4 min-h-4 w-4 min-w-4 hover:scale-[115%] active:scale-95"
+          className="h-4 min-h-5 w-4 min-w-5 hover:scale-[115%] active:scale-95"
           src="./kebabMenu.svg"
           alt="kebab menu for accessing chat settings"
         />
